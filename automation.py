50c87afea9a99f74a2a8b57690b0851952188813
SuguruOoki <suguru.oki.programming@gmail.com>
2017-07-13 14:20:43 +0900

    フォルダ内csvファイルの検出機能追加

--- automation.py
+++ automation.py
@@ -5,7 +5,6 @@ import bz2,os,sys,glob,re,requests,json
 class Postal():
     def getAdressByPostalCode(postal_code):
         url = 'http://geoapi.heartrails.com/api/json'
-        postal_code = "999-2224"
         postal_code_length = len(re.findall('\d',postal_code))
 
         # 郵便番号のバリデーション
@@ -33,13 +32,34 @@ class ExcelProcess():
             for file in files:
                 yield os.path.join(root, file)
 
-    # for file in fild_all_files(root_folder):
-        # root, ext = os.path.splitext(file)
 
-        # if ext == '.bz2':
-        #     # print(os.path.basename(file))
-        #     print(file)
-        #     f = bz2.BZ2File(file).read()
+class CsvProcess():
+    def find_all_files(self,directory):
+        print(directory)
+        for root, dirs, files in os.walk(directory):
+            print(root)
+            print(dirs)
+            print(files)
+            yield root
+            for file in files:
+                yield os.path.join(root, file)
+                print(file)
+
+        for file in fild_all_files(root_folder):
+            root, ext = os.path.splitext(file)
+            print(file)
+
+    def get_files(self,directory):
+        files = os.listdir(directory+"/t_townwork")
+        for file in files:
+            root, ext = os.path.splitext(file)
+            if ext == '.csv':
+                print(file)
+
+            # if ext == '.bz2':
+            #     print(os.path.basename(file))
+            #     print(file)
+            #     f = bz2.BZ2File(file).read()
             # src ="" # これが置き換え用の変数
             # if "(株)" in src:
             #     dst = src.replace("(株)", "株式会社")
@@ -53,4 +73,8 @@ class ExcelProcess():
             # f.close()
 
 if __name__ == '__main__':
-    Postal.getAdressByPostalCode("999-2224")
+    # Postal.getAdressByPostalCode("164-0014")
+    root_folder = os.getcwd()
+    # print(root_folder)
+    csv = CsvProcess()
+    csv.get_files(root_folder)
