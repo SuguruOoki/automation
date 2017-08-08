a82d756fecf29d7de6fce174cdf991293cfc7446
SuguruOoki <suguru.oki.programming@gmail.com>
2017-08-08 16:25:32 +0900

    add a comment in exist_mv.pl

--- perl/exist_mv.pl
+++ perl/exist_mv.pl
@@ -1,15 +1,21 @@
 #!/usr/bin/perl
+# おそらくTABAITAIと付いているファイル(処理がうまくいかなかったファイル)を
+# 一つの別フォルダにまとめて移している処理をしているスクリプトだと思われる。
+
 use strict;
 use warnings;
 
+# これらは何？
 my $before = $ARGV[0];
 my $after = $ARGV[1];
 my $mv_flg = $ARGV[2];
 
+# これは処理をするディレクトリとやるかどうかのフラグだと思われる。
 my $before_dir = '/home/lamp_app/crm_source/codes/tmp/csv/Import/before/';
 my $after_dir = '/home/lamp_app/crm_source/codes/tmp/csv/Import/after/';
 my $exist_after = 0;
 
+# 処理をする前のディレクトリを開き、その中に処理後のディレクトリが存在したら処理後だと判断しexist_afterのフラグを立てる。
 opendir(NEW,$before_dir)or die "$!";
 while(my $dir_1 = readdir(NEW)){
 	next unless($dir_1 eq $after);
@@ -17,12 +23,14 @@ while(my $dir_1 = readdir(NEW)){
 } #while $dir_1
 close(NEW);
 
+# 処理後でなければ、まず処理後のものを入れるディレクトリを作成する。
 unless($exist_after){
 	system("mkdir $before_dir"."$after");
 } #unless
 
 my @aray;
 
+# before_dirディレクトリ内にあるbeforeのディレクトリの中を見てTABAITAIと接頭辞が付いているxlsxファイルがあればaray配列に保存する。
 opendir(BEFORE,$before_dir.$before)or die "$!";
 while(my $dir_before = readdir(BEFORE)){
 	next unless($dir_before =~ /TABAITAI.*\.xlsx?/);
@@ -31,22 +39,31 @@ while(my $dir_before = readdir(BEFORE)){
 close(BEFORE);
 
 my @list;
-my %debug;
+my %debug; # %debugってなんだ？？？
+
+# リネームをしているらしい。
+# after_dirディレクトリ内にあるbeforeディレクトリを開く。
+# 開いたディレクトリを読んでTABAITAIと付いているcsvファイルがあれば
+# listに何やらファイル名と共にフラグを入れているらしい。
 opendir(AFTER,$after_dir.$before)or die "$!";
 while(my $dir_after = readdir(AFTER)){
 	next unless($dir_after =~ /TABAITAI.*\.csv/);
 	for(my $i=0;$i<@aray;$i++){
 	my $excel;
 	my $csv;
-	unless(defined($list[$i]->[0])){$list[$i]->[0] = $aray[$i]};
-	unless(defined($list[$i]->[1])){$list[$i]->[1] = '0'};
-	unless(defined($csv)){$csv = 'undef'};
-	unless(defined($excel)){$excel = 'undef'};
-	
-	$debug{$list[$i]->[0]} = 0;
-	$debug{$dir_after} = 1;
 
-		if($list[$i]->[0] =~ /(TABAITAI__.*_[0-9]{1,}_[0-9]{1,}_[0-9]{1,}_[0-9]{1,})/){
+	# 扱っているのは二次元配列の模様。四つのunlessを乗り越えてから色々処理が走るらしい。
+	# definedは定義済みならtrueを返す
+	# unlessは条件式が偽の時に{}内の処理を走らせる。
+	unless(defined($list[$i]->[0])){$list[$i]->[0] = $aray[$i]}; # listの[0]が未定義なら定義する
+	unless(defined($list[$i]->[1])){$list[$i]->[1] = '0'}; # listの[1]が未定義なら定義する。これは処理後かどうかを調べる際のフラグと思われる。
+	unless(defined($csv)){$csv = 'undef'}; # csvが未定義ならundefを代入
+	unless(defined($excel)){$excel = 'undef'}; # excelが未定義ならundefを代入
+
+	$debug{$list[$i]->[0]} = 0; # 連想配列を定義する。listに入れたファイル名をキーにしてフラグを0に設定する。
+	$debug{$dir_after} = 1; # 処理後のファイルを入れるディレクトリパスをキーにしてフラグを立てておく。
+
+		if($list[$i]->[0] =~ /(TABAITAI__.*_[0-9]{1,}_[0-9]{1,}_[0-9]{1,}_[0-9]{1,})/){#
 			$excel = $1;
 		}elsif($list[$i]->[0] =~ /(TABAITAI__.*_[0-9]{1,}_[0-9]{1,}_[0-9]{1,})/){
 			$excel = $1;
@@ -61,18 +78,18 @@ while(my $dir_after = readdir(AFTER)){
 		}elsif($dir_after =~ /(TABAITAI__.*_[0-9]{1,}_[0-9]{1,})/){
 			$csv = $1;
 		} #if excel
-	
+
 		if($excel eq $csv){
-			$list[$i]->[1] = '1';
+			$list[$i]->[1] = '1'; # csvとexcelファイルでファイル名が同じものが存在するようならばフラグを立てておく。
 		} #if
 	} #for
 } #while $dir_before
 close(BEFORE);
 
-my $border = '-' x 100;
+my $border = '-' x 100; # printの際にファイル名の間に入れるボーダーと思われる。
 foreach my $key (sort(keys(%debug))){
 	if($key =~ /\.xls/){
-		print $border,"\n";	
+		print $border,"\n";
 		print $key,"\n";
 	}else{
 		print"⇒",$key,"\n";
@@ -87,27 +104,3 @@ for(my $j=0;$j<@list;$j++){
 	} #if
 } #for
 }#if
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
